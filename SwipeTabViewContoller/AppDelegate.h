0840fba9ed78c71f84af4c878eb6788a296acfa6
小小 <lucky@xiaoxiaodembp.lan>
2016-04-06 18:38:53 +0800

    Initial Commit

---
+++ SwipeTabViewContoller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwipeTabViewContoller
+//
+//  Created by 小小 on 16/4/6.
+//  Copyright © 2016年 小小. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwipeTabViewContoller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwipeTabViewContoller
+//
+//  Created by 小小 on 16/4/6.
+//  Copyright © 2016年 小小. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
